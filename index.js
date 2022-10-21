bf1142502afeff85d4a64efe458c2d699f9d2e3b
Mohit Yadav <www.mohit2004@gmail.com>
2022-10-21 19:16:51 +0530

    export module

--- index.js
+++ index.js
@@ -44,7 +44,9 @@ app.post("/shortUrl", async (req, res) => {
   res.json({ shortUrl: link.shortUrl });
 });
 
-// Start the server
-app.listen(3000, () => {
-  console.log("Server started on port 3000");
-});
+// // Start the server
+// app.listen(3000, () => {
+//   console.log("Server started on port 3000");
+// });
+
+module.exports = app;
